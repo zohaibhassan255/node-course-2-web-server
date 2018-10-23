84f9e1d0c227456211290bca2c4aa87b406bfb61
Apple <apple@Apples-iMac.local>
2018-10-23 23:00:05 +0500

    Setup Start script and Heroku Port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -13,7 +15,7 @@ app.use((req , res , next) => {
   console.log(log);
   fs.appendFile('server.log' , log + '\n' , (err) => {
     if (err) {
-      console.log('Error is writing filie.');
+      console.log('Error is writing file.');
     }
   });
   next();
@@ -52,6 +54,6 @@ app.get('/bad' , (req , res) => {
   });
 });
 
-app.listen(3000 , () => {
-  console.log('Server is up on port 3000');
+app.listen(port , () => {
+  console.log(`Server is up on port ${port}`);
 });
