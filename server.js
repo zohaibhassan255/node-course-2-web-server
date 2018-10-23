5ea4e811561c8bff26e82a31f7f67f12c2f4ad0c
Apple <apple@Apples-iMac.local>
2018-10-23 23:19:39 +0500

    Added Projects Page

--- server.js
+++ server.js
@@ -48,6 +48,12 @@ app.get('/about' , (req , res) => {
   });
 });
 
+app.get('/projects' , (req , res) => {
+  res.render('projects.hbs' , {
+    pageTitle: 'Projects Page'
+  });
+});
+
 app.get('/bad' , (req , res) => {
   res.send({
     errorMessage: 'There is an error!'
